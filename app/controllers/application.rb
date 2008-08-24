1c7797dc5142f6d403be334a92c12d00fd72c229
wpc <alex.hal9000@gmail.com>
2008-08-24 20:56:43 +0800

    add player controller and add footnote plugin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'a37c0c19e659ed041742bd4820646087'
+  protect_from_forgery :secret => 'a37c0c19e659ed041742bd4820646090'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,10 @@
 class PlayersController < ApplicationController
+  def login
+  end
+  
+  def attach
+    session['player'] = Player.find_or_create_by_name(params[:name]).id
+    flash[:notice] =  "login successful!"
+    render :template => "players/login"
+  end
 end
